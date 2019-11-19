3c500af90f0c647f6cd99495bed5df10bb0efc6b
ganeshteja36 <ganeshteja36@gmail.com>
2019-11-19 10:55:18 +0530

    Initial commit

---
+++ website/webapp/urls.py
@@ -0,0 +1,25 @@
+"""website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('view/<int:pk>', views.book_view, name='book_view'),
+    path('new', views.book_create, name='book_new'),
+    path('edit/<int:pk>', views.book_update, name='book_edit'),
+    path('delete/<int:pk>', views.book_delete, name='book_delete'),
+]
